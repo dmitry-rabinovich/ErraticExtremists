d6e30f8ccd8104218776a89fb6469e78a77ba2f1
dmitry-rabinovich <56108421+dmitry-rabinovich@users.noreply.github.com>
2019-10-30 09:30:22 +0200

    add manual save to tex in plotter
    tex code is needed to present custom colors per point in scatter graphs
    see https://tex.stackexchange.com/questions/197270/how-to-plot-scatter-points-using-pgfplots-with-color-defined-from-table-rgb-valu

--- ExcessiveExtremists.m
+++ ExcessiveExtremists.m
@@ -1,13 +1,13 @@
 % model parameters
-N           = 61;   % the number of agents
-N_LEFT      = 5;    % the number of left extremists (default: 1)
+N           = 21;   % the number of agents
+N_LEFT      = 3;    % the number of left extremists (default: 1)
 N_RIGHT     = 4;	% the number of right extremists (default: 1)
 epsilon     = 0.1;
 eps_left    = epsilon;	% the probability of left extremists to become even more radical
 eps_right   = epsilon;	% the probability of right extremists to become even more radical
 
 % simulation parameters
-memory_length       = 400;  % how long should we remember things
+memory_length       = 200;  % how long should we remember things
 initialRangeLeft    = -10;   % initial range left boundary of possible values 
 initialRangeRight   = 10;	% initial range right boundary of possible values
 
@@ -39,4 +39,4 @@ if show_final_only
     plotter.Show(points);
 end
 
-plotter.Save('images/excessive.extremists.tex');
+plotter.SaveDirectlyToTex('images/excessive.extremists.tex', points);
